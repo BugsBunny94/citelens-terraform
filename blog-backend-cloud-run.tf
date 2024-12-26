4084baf820dc271cf0d69217ccd8e6f9dec7a655
Aymane <aymane.bouziane.pro@gmail.com>
2024-12-26 19:30:40 +0100

    add blog backend cloud run

---
+++ blog-backend-cloud-run.tf
@@ -0,0 +1,48 @@
+locals {
+  redeploy_blog_backend_cloud_run = true
+  count_blog_backend_cloud_run = 1
+}
+
+resource "google_cloud_run_service" "blog_backend_cloud_run" {
+  name     = "blog-backend"
+  location = local.region
+  count    = local.count_blog_backend_cloud_run
+
+  template {
+    metadata {
+      annotations = {
+        "force-redeploy" = local.redeploy_blog_backend_cloud_run ? timestamp() : 0 
+      }
+    }
+    spec {
+      containers {
+        image = "${local.region}-docker.pkg.dev/${local.project_id}/${google_artifact_registry_repository.my_repository.name}/blogws" 
+        resources {
+          limits = {
+            memory = "512Mi"
+            cpu    = "1"
+          }
+        }
+        env {
+          name  = "SPRING_PROFILES_ACTIVE"
+          value = "production"
+        }        
+      }
+      timeout_seconds = 300
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+# Autoriser l'accès public à Cloud Run
+resource "google_cloud_run_service_iam_member" "blog_invoker" {
+  count    = local.count_blog_backend_cloud_run > 0 ? 1 : 0
+  service  = google_cloud_run_service.blog_backend_cloud_run[0].name
+  location = google_cloud_run_service.blog_backend_cloud_run[0].location
+  role     = "roles/run.invoker"
+  member   = "allUsers" # Rendre le service accessible publiquement
+}
